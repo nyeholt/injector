aab6afc31a0fd3e781e85fc97601e79e11da13b8
Marcus Nyeholt <nyeholt@gmail.com>
2013-12-05 09:48:23 +1100

    Added ability to bind multiple aspects for a single method in an AopProxy

--- src/November/Aspects/AopProxyService.php
+++ src/November/Aspects/AopProxyService.php
@@ -15,14 +15,28 @@ class AopProxyService {
 	public $afterCall = array();
 
 	public $proxied;
+	
+	/**
+	 * Because we don't know exactly how the proxied class is usually called,
+	 * provide a default constructor
+	 */
+	public function __construct() {
+		
+	}
 
 	public function __call($method, $args) {
 		if (method_exists($this->proxied, $method)) {
 			$continue = true;
 			if (isset($this->beforeCall[$method])) {
-				$result = $this->beforeCall[$method]->beforeCall($this->proxied, $method, $args);
-				if ($result === false) {
-					$continue = false;
+				$methods = $this->beforeCall[$method];
+				if (!is_array($methods)) {
+					$methods = array($methods);
+				}
+				foreach ($methods as $handler) {
+					$result = $handler->beforeCall($this->proxied, $method, $args);
+					if ($result === false) {
+						$continue = false;
+					}
 				}
 			}
 
@@ -30,11 +44,17 @@ class AopProxyService {
 				$result = call_user_func_array(array($this->proxied, $method), $args);
 			
 				if (isset($this->afterCall[$method])) {
-					$this->afterCall[$method]->afterCall($this->proxied, $method, $args, $result);
+					$methods = (array) $this->afterCall[$method];
+					if (!is_array($methods)) {
+						$methods = array($methods);
+					}
+					foreach ($methods as $handler) {
+						$handler->afterCall($this->proxied, $method, $args, $result);
+					}
 				}
 
 				return $result;
 			}
-        }
+		}
 	}
 }
